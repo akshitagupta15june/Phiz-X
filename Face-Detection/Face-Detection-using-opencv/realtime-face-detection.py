cc56d82ad4340615acbdb08fc8e20c51c06f412f
Abhishek Seth <54003019+abs12seth@users.noreply.github.com>
2021-02-22 16:52:59 +0530

    Update realtime-face-detection.py

--- Face-Detection/Face-Detection-using-opencv/realtime-face-detection.py
+++ Face-Detection/Face-Detection-using-opencv/realtime-face-detection.py
@@ -1,7 +1,13 @@
 import cv2 as cv
 face_cascade=cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+import cv2
+from matplotlib import pyplot as plt
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+cas = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
 
 cap=cv.VideoCapture('bts.mp4')
+image_frame = []
 while cap.isOpened():
     _,img=cap.read()
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -10,6 +16,23 @@ while cap.isOpened():
         cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)
     cv.imshow('lovely', img)
     if cv.waitKey(1) &0xFF==ord('q'):
+    _, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    detected_faces = face_cascade.detectMultiScale(gray)
+    for (x, y, w, h) in detected_faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        face_frame_gray = gray[y:y+h, x:x+w]
+        face_frame_img = img[y:y + h, x:x + w]
+        eyes = cas.detectMultiScale(face_frame_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(face_frame_img, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 2)
+
+    image_frame.append(img)
+    cv2.imshow('img', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
+cv2.destroyAllWindows()
+
